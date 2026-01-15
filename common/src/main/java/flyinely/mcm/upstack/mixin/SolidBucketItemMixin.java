15f10fbd15396b462f89b78651d53b34a0c5e941
FlyInEly <jw.32640@gmail.com>
2026-01-14 20:56:18 -0800

    remove unnecessary mixins to fix issues with fluid bucket stacks of size >1; seems fixed in vanilla 1.21.1

---
+++ common/src/main/java/flyinely/mcm/upstack/mixin/SolidBucketItemMixin.java
@@ -0,0 +1,8 @@
+package flyinely.mcm.upstack.mixin;
+
+import net.minecraft.world.item.SolidBucketItem;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(SolidBucketItem.class)
+public class SolidBucketItemMixin {
+}
--- common/src/main/java/flyinely/mcm/upstack/registry/UpstackStackSizes.java
+++ common/src/main/java/flyinely/mcm/upstack/registry/UpstackStackSizes.java
@@ -1,15 +1,34 @@
 package flyinely.mcm.upstack.registry;
 
 import flyinely.mcm.upstack.util.ItemComponentUtil;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 
 public class UpstackStackSizes {
 
    // Only need to use the ItemPredicateRegistry for rules that aren't per-item.
 
+   /*
+    * 1.21.1 works out of the box for:
+    * - Fluid buckets, e.g. water, tadpole
+    * - Milk buckets
+    *
+    * And needs mixins for:
+    * - Solid buckets, e.g. powder snow
+    */
    public static void register() {
-      ItemComponentUtil.setMaxStackSize(Items.WATER_BUCKET, 16); // TODO: get from config
+      ItemComponentUtil.setMaxStackSize(Items.WATER_BUCKET, 16); // TODO: get each from config
+      ItemComponentUtil.setMaxStackSize(Items.LAVA_BUCKET, 16);
+      ItemComponentUtil.setMaxStackSize(Items.PUFFERFISH_BUCKET, 16);
+      ItemComponentUtil.setMaxStackSize(Items.SALMON_BUCKET, 16);
+      ItemComponentUtil.setMaxStackSize(Items.COD_BUCKET, 16);
+      ItemComponentUtil.setMaxStackSize(Items.TROPICAL_FISH_BUCKET, 16);
+      ItemComponentUtil.setMaxStackSize(Items.AXOLOTL_BUCKET, 16);
+      ItemComponentUtil.setMaxStackSize(Items.TADPOLE_BUCKET, 16);
+      ItemComponentUtil.setMaxStackSize(Items.MILK_BUCKET, 16);
 
+      // Needs fix
+      ItemComponentUtil.setMaxStackSize(Items.POWDER_SNOW_BUCKET, 16);
    }
 
 }
