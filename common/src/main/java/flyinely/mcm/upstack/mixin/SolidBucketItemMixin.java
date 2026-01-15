5a589aea82d388353878fcd068324142994ad8b6
FlyInEly <jw.32640@gmail.com>
2026-01-15 11:37:15 -0800

    add mixin to fix SolidBucketItem issues with variable stack size

--- common/src/main/java/flyinely/mcm/upstack/mixin/SolidBucketItemMixin.java
+++ common/src/main/java/flyinely/mcm/upstack/mixin/SolidBucketItemMixin.java
@@ -1,8 +1,50 @@
 package flyinely.mcm.upstack.mixin;
 
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.BucketItem;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SolidBucketItem;
+import net.minecraft.world.item.context.UseOnContext;
+import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
+/**
+ * Common-side mixin.
+ * <p>
+ * Fixes issues due to {@link SolidBucketItem} assuming that it is not stackable.
+ */
 @Mixin(SolidBucketItem.class)
 public class SolidBucketItemMixin {
+
+   /**
+    * When placing a {@link SolidBucketItem} without infinite materials:
+    * <ul>
+    *    <li>
+    *       Replace the filled stack with the empty stack <i>only if</i> the filled stack has no items. (It has already been decremented in
+    *       {@link net.minecraft.world.item.BlockItem#useOn(UseOnContext)}, which {@link SolidBucketItem#useOn(UseOnContext)} calls before this redirect.)
+    *    </li>
+    *    <li>
+    *       Otherwise, try to add the empty stack to the inventory, dropping the stack if not possible.
+    *    </li>
+    * </ul>
+    */
+   @Redirect(method = "useOn", at = @At(
+         value = "INVOKE",
+         target = "Lnet/minecraft/world/item/BucketItem;getEmptySuccessItem(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/entity/player/Player;)Lnet/minecraft/world/item/ItemStack;"))
+    ItemStack preserveFilledStack(ItemStack filledStack, @NotNull Player player) {
+      // This check is to ensure that when player.hasInfiniteMaterials(), we do not duplicate filledStack
+      // by adding getEmptySuccessItem() == filledStack to the inventory.
+      if (!player.hasInfiniteMaterials()) {
+         ItemStack emptyStack = BucketItem.getEmptySuccessItem(filledStack, player);
+         if (filledStack.isEmpty()) {
+            return emptyStack;
+         } else if (!player.getInventory().add(emptyStack)) {
+            player.drop(emptyStack, false);
+         }
+      }
+      return filledStack;
+   }
+
 }
--- common/src/main/java/flyinely/mcm/upstack/registry/UpstackStackSizes.java
+++ common/src/main/java/flyinely/mcm/upstack/registry/UpstackStackSizes.java
@@ -12,12 +12,14 @@ public class UpstackStackSizes {
     * 1.21.1 works out of the box for:
     * - Fluid buckets, e.g. water, tadpole
     * - Milk buckets
+    * - Potions
     *
     * And needs mixins for:
     * - Solid buckets, e.g. powder snow
     */
-   public static void register() {
-      ItemComponentUtil.setMaxStackSize(Items.WATER_BUCKET, 16); // TODO: get each from config
+   public static void register() {// TODO: get each from config
+      // buckets
+      ItemComponentUtil.setMaxStackSize(Items.WATER_BUCKET, 16);
       ItemComponentUtil.setMaxStackSize(Items.LAVA_BUCKET, 16);
       ItemComponentUtil.setMaxStackSize(Items.PUFFERFISH_BUCKET, 16);
       ItemComponentUtil.setMaxStackSize(Items.SALMON_BUCKET, 16);
@@ -26,9 +28,14 @@ public class UpstackStackSizes {
       ItemComponentUtil.setMaxStackSize(Items.AXOLOTL_BUCKET, 16);
       ItemComponentUtil.setMaxStackSize(Items.TADPOLE_BUCKET, 16);
       ItemComponentUtil.setMaxStackSize(Items.MILK_BUCKET, 16);
-
-      // Needs fix
       ItemComponentUtil.setMaxStackSize(Items.POWDER_SNOW_BUCKET, 16);
+
+      // potions
+      ItemComponentUtil.setMaxStackSize(Items.POTION, 16);
+      ItemComponentUtil.setMaxStackSize(Items.SPLASH_POTION, 4); // TODO: add cooldown
+      ItemComponentUtil.setMaxStackSize(Items.LINGERING_POTION, 4);
+
+
    }
 
 }
