2cfcb788d76303d4fdce687e5e772f404305d3c6
FlyInEly <jw.32640@gmail.com>
2026-01-26 11:12:19 -0800

    actually, include fzzy_config on the common classpath

--- fabric/src/main/java/flyinely/mcm/upstack/UpstackFabric.java
+++ fabric/src/main/java/flyinely/mcm/upstack/UpstackFabric.java
@@ -4,7 +4,7 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 
 public class UpstackFabric implements ModInitializer {
-
+   
    @Override
    public void onInitialize() {
       // Use Fabric to bootstrap the Common mod.
