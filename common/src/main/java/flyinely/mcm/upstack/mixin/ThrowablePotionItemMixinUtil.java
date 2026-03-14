216d08ce6ee6fd7963d10af63d4d6a9bf29ab106
FlyInEly <jw.32640@gmail.com>
2026-03-14 15:48:04 -0700

    [unstable] work on splash and lingering potion joint cooldown

---
+++ common/src/main/java/flyinely/mcm/upstack/mixin/ThrowablePotionItemMixinUtil.java
@@ -0,0 +1,50 @@
+package flyinely.mcm.upstack.mixin;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import flyinely.mcm.upstack.config.Config;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.*;
+import net.minecraft.world.level.Level;
+
+/**
+ * TODO: Fix these temporary docs
+ * Sets and enforces a joint use cooldown for {@link Items#SPLASH_POTION} and {@link Items#LINGERING_POTION}
+ * using the cooldown duration (in ticks) supplied by {@link Config.Cooldowns#POTION_THROW_COOLDOWN}, if greater
+ * than zero.
+ *
+ * @implNote For optimization and safety, it is assumed that the used item is either a {@link Items#SPLASH_POTION}
+ * or {@link Items#LINGERING_POTION}, and that its cooldown thus reflects the joint cooldown. The {@link Items#SPLASH_POTION}
+ * and {@link Items#LINGERING_POTION} cooldowns are set, and the used item's cooldown is enforced.
+ */
+class ThrowablePotionItemMixinUtil {
+
+   /**
+    * Sets and enforces a joint use cooldown for {@link Items#SPLASH_POTION} and {@link Items#LINGERING_POTION}
+    * using the cooldown duration (in ticks) supplied by {@link Config.Cooldowns#POTION_THROW_COOLDOWN}, if greater
+    * than zero.
+    *
+    * @implNote For optimization and safety, it is assumed that the used item is either a {@link Items#SPLASH_POTION}
+    * or {@link Items#LINGERING_POTION}, and that its cooldown thus reflects the joint cooldown. The {@link Items#SPLASH_POTION}
+    * and {@link Items#LINGERING_POTION} cooldowns are set, and the used item's cooldown is enforced.
+    */
+   static InteractionResultHolder<ItemStack> useWithCooldown(Level level, Player player, InteractionHand hand, Operation<InteractionResultHolder<ItemStack>> original) {
+      int cooldownTicks = Config.Cooldowns.POTION_THROW_COOLDOWN.getAsInt();
+      if (cooldownTicks > 0) {
+            ItemStack stack = player.getItemInHand(hand);
+            ItemCooldowns cooldowns = player.getCooldowns();
+            // Get only this item's cooldown (optimization, safety)
+            if (cooldowns.isOnCooldown(stack.getItem())) {
+               // Exit early if currently on cooldown
+               return InteractionResultHolder.sidedSuccess(stack, level.isClientSide());
+            }
+            // Set both items' cooldown
+            cooldowns.addCooldown(Items.SPLASH_POTION, cooldownTicks);
+            cooldowns.addCooldown(Items.LINGERING_POTION, cooldownTicks);
+      }
+      // Call the original behavior (not currently on cooldown)
+      return original.call(level, player, hand);
+   }
+
+}
