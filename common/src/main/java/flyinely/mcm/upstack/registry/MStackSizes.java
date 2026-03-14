1c2ce533aba4543bfa66f3ca0267159fa1b0c31e
FlyInEly <jw.32640@gmail.com>
2026-03-13 19:32:37 -0700

    add pastel tags and stack size modifiers (without configs!)

--- common/src/main/java/flyinely/mcm/upstack/registry/MStackSizes.java
+++ common/src/main/java/flyinely/mcm/upstack/registry/MStackSizes.java
@@ -2,10 +2,12 @@ package flyinely.mcm.upstack.registry;
 
 import flyinely.mcm.upstack.util.ItemComponentUtil;
 import flyinely.mcm.upstack.util.ResUtil;
+import flyinely.mcm.upstack.util.TagUtil;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.world.item.Items;
 
 import static flyinely.mcm.upstack.config.Config.*;
+import static flyinely.mcm.upstack.util.ResUtil.id;
 
 // Server side
 public class MStackSizes {
@@ -75,8 +77,38 @@ public class MStackSizes {
       ItemComponentUtil.setMaxStackSize(ItemTags.BANNERS, StackSize.BANNERS.get());
 
       // testing
-      // warning: may currently be the default item
-      ItemComponentUtil.setMaxStackSize(ResUtil.id("pastel:resonance_shard"), 64);
+      ItemComponentUtil.setMaxStackSize(MItemTags.Pastel.BULBS,64); // 16
+      ItemComponentUtil.setMaxStackSize(MItemTags.Pastel.FUSION_SHRINES,64); // 1
+      ItemComponentUtil.setMaxStackSize(MItemTags.Pastel.ITEM_BOWLS,64); // 16 (enlightenment: 64)
+      ItemComponentUtil.setMaxStackSize(MItemTags.Pastel.NETWORK_NODES,64); // 16
+      ItemComponentUtil.setMaxStackSize(MItemTags.Pastel.PEDESTALS,64); // 1
+      ItemComponentUtil.setMaxStackSize(MItemTags.Pastel.ROUNDELS,64); // 16 (preservation: 64)
+      ItemComponentUtil.setMaxStackSize(MItemTags.Pastel.SHOOTING_STARS,16); // 1; spawns entity
+      ItemComponentUtil.setMaxStackSize(MItemTags.Pastel.STRUCTURE_UPGRADES,64); // 16
+      ItemComponentUtil.setMaxStackSize(id("pastel:aether_vestiges"),64); // 1 (test multiple to repair nectar lance in anvil!)
+      ItemComponentUtil.setMaxStackSize(id("pastel:bag_of_holding"), 0); // (1)
+      ItemComponentUtil.setMaxStackSize(id("pastel:cinderhearth"), 64); // 1
+      ItemComponentUtil.setMaxStackSize(id("pastel:clotted_cream"), 64); // 64, but in #c:buckets (why??) so needs override AFTER buckets is set
+      ItemComponentUtil.setMaxStackSize(id("pastel:color_picker"), 64); // 8
+      ItemComponentUtil.setMaxStackSize(id("pastel:crystal_apothecary"), 64); // 8
+      ItemComponentUtil.setMaxStackSize(id("pastel:crystallarieum"), 64); // 1
+      ItemComponentUtil.setMaxStackSize(id("pastel:downstone_fragments"), 64); // 16
+      ItemComponentUtil.setMaxStackSize(id("pastel:dragonbone_broth"), 16); // 8. grants magic annulation
+      ItemComponentUtil.setMaxStackSize(id("pastel:enchanter"), 64); // 1
+      ItemComponentUtil.setMaxStackSize(id("pastel:ender_splice"), 0); // (16)
+      ItemComponentUtil.setMaxStackSize(id("pastel:jadeite_lotus_flower"), 64); // 16
+      ItemComponentUtil.setMaxStackSize(id("pastel:lucky_roll"), 0); // (16)
+      ItemComponentUtil.setMaxStackSize(id("pastel:moonstone_core"), 64); // 16
+      ItemComponentUtil.setMaxStackSize(id("pastel:palteria_gem"), 64); // 16
+      ItemComponentUtil.setMaxStackSize(id("pastel:resonance_shard"), 64); // 16
+      ItemComponentUtil.setMaxStackSize(id("pastel:spirit_instiller"), 64); // 1
+      ItemComponentUtil.setMaxStackSize(id("pastel:star_fragment"), 64); // 16
+      ItemComponentUtil.setMaxStackSize(id("pastel:stardust_block"), 64); // 1
+      ItemComponentUtil.setMaxStackSize(id("pastel:stratine_gem"), 64); // 16
+      ItemComponentUtil.setMaxStackSize(id("pastel:triple_meat_pot_pie"), 16); // 8
+      ItemComponentUtil.setMaxStackSize(id("pastel:triple_meat_pot_stew"), 16); // 8
+
+
    }
 
 }
