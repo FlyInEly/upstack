e774b7b634ef3d815ba2e4cbd459c4d7f73bb7f6
JW3264 <JW.32640@gmail.com>
2026-03-21 21:47:00 -0700

    ⚙️ Streamline slot targeting logic from saddle fix (parity with banner pattern fix)

--- common/src/main/java/flyinely/mcm/upstack/mixin/HorseInventoryMenuMixin.java
+++ common/src/main/java/flyinely/mcm/upstack/mixin/HorseInventoryMenuMixin.java
@@ -2,30 +2,25 @@ package flyinely.mcm.upstack.mixin;
 
 
 import flyinely.mcm.upstack.Constants;
-import net.minecraft.world.Container;
-import net.minecraft.world.entity.animal.horse.AbstractHorse;
-import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.inventory.HorseInventoryMenu;
 import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArgs;
-import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 /**
  * Common-side mixin.
  * Fixes issues due to the vanilla assumption that {@link net.minecraft.world.item.SaddleItem} is not stackable.
  * Affects horses, mules, and donkeys (but not striders).
  * <ul>
- * <li>Slot 0 holds up to 64 saddles. Issue: Equipping >1 saddle is redundant.</li>
- * <li>Slot 1 holds up to 1 horse armor. No issue.</li>
- * <li>Slots 2+ hold up to 64 inventory items each. No issue.</li>
+ * <li>Container slot 0 holds up to 64 saddles, but equipping >1 saddle is redundant.</li>
+ * <li>Container slot 1 holds up to 1 horse armor (no issue).</li>
+ * <li>Container slots 2+ hold up to 64 inventory items each (no issue).</li>
  * </ul>
  */
 @Mixin(HorseInventoryMenu.class)
@@ -39,30 +34,19 @@ public abstract class HorseInventoryMenuMixin {
 	@Unique
 	private static final Logger upstack$LOG = LoggerFactory.getLogger(Constants.LOG.getName() + "/" + HorseInventoryMenuMixin.class.getSimpleName());
 	
-	@ModifyArgs(
+	@ModifyArg(
 			method = "<init>",
 			at = @At(
 					value = "INVOKE",
 					target = "Lnet/minecraft/world/inventory/HorseInventoryMenu;addSlot(Lnet/minecraft/world/inventory/Slot;)Lnet/minecraft/world/inventory/Slot;"
 			)
 	)
-	void modifyMaxStackSize(Args args, int containerId, Inventory inventory, Container horseContainer, final AbstractHorse horse, int columns) {
-		
-		// Safeguard against unsafe mixin
-		if (args.size() != 1) {
-			upstack$LOG.error("Failed to restrict the max stack size of horse saddle slot (index: 0, container slot: 0): unexpected argument count");
-			return;
-		}
+	Slot modifyMaxStackSize(Slot original) {
 		
 		// Modify saddle slot only
-		//		- Index 0, container slot 0.
-		//		- Does not accept horse armor. Use "!original.mayPlace(Items.LEATHER_HORSE_ARMOR...)" rather than
-		//		  "original.mayPlace(Items.SADDLE...)" because the former depends only on ItemStack#is(), whereas the
-		//		  latter also depends on Slot#hasItem() and AbstractHorse#isSaddleable().
-		final Slot original = args.get(0);
-		if (original.index == 0 && original.getContainerSlot() == 0 && !original.mayPlace(Items.LEATHER_HORSE_ARMOR.getDefaultInstance())) {
-			upstack$LOG.debug("Restricting the max stack size of the horse saddle slot (index: 0, container slot: 0).");
-			args.set(0, new Slot(original.container, original.getContainerSlot(), original.x, original.y) {
+		if (original.getContainerSlot() == 0) {
+			upstack$LOG.debug("Restricting the max stack size of the horse saddle slot (container slot: 0).");
+			return new Slot(original.container, original.getContainerSlot(), original.x, original.y) {
 				
 				// Override max stack size
 				
@@ -76,9 +60,9 @@ public abstract class HorseInventoryMenuMixin {
 				
 				@Override
 				public boolean isActive() { return original.isActive(); }
-			});
+			};
 		}
-		
+		return original;
 	}
 	
 }
