cede3ed3bc5c04c31f1db1b8f1113e37f57fe14c
FlyInEly <70659753+FlyInEly@users.noreply.github.com>
2026-06-05 15:01:54 -0700

    Revert "🍎 Add config folder comments"
    This reverts commit 11c435a4153d0025863280ce18f373f5be815d00.

--- common/src/main/java/flyinely/mcm/upstack/config/Config.java
+++ common/src/main/java/flyinely/mcm/upstack/config/Config.java
@@ -44,7 +44,7 @@ public class Config {
       public static final IntValue OVERSTACKED_COLOR; // LATER: Use a ConfigValue which safely accepts direct hex.
 
       static {
-         BUILDER.comment("Configure indicators for overstacked items: stacks whose count exceeds the max stack size.").push("display");
+         BUILDER.push("display");
 
          OVERSTACKED_SYMBOL = BUILDER
                .comment("Overlay a \"#\" symbol on overstacked items: stacks whose count exceeds the max stack size.")
@@ -325,7 +325,7 @@ public class Config {
       }
 
       static {
-         BUILDER.comment("Configure the max stack sizes of supported items.").push("stack_size");
+         BUILDER.push("stack_size");
 
          Common.init();
          Pastel.init();
